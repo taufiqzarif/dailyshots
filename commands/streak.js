120ed5a00c93ddf15ea992d61d496e5e286a25b8
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-11-11 18:27:06 +0800

    Small change

--- commands/streak.js
+++ commands/streak.js
@@ -6,7 +6,12 @@ module.exports = {
         .setDescription('Shows the streak of a user.'),
     async execute(interaction, client) {
         await interaction.deferReply();
-        const user = interaction.options.getMember('target');
+
+        // THE LOGIC
+        // Sending an image per day will increment 1 of the user's streak points.
+        // After 3 days of sending images in the channel, update and add an emoji 🔥 streak to the user's nickname.
+        // User can enable disable the emoji nickname feature.
+        const user = interaction.user.tag;
         await interaction.editReply(user);
     },
 };
