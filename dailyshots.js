1eba72704e8a08b2b67d0445ddebed969f929204
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-11-13 16:34:15 +0800

    Added subfolders command with working deploy commands

---
+++ dailyshots.js
@@ -0,0 +1,107 @@
+// Require the necessary discord.js classes
+const dotenv = require('dotenv').config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { readdir } = require('node:fs');
+const { token, clientId, guildId } = process.env;
+
+console.log(token, clientId, guildId);
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "./commands/");
+console.log(commandsPath);
+let folders = fs.readdirSync(commandsPath);
+folders.forEach(sub => {
+    const commandFiles = fs
+    .readdirSync(commandsPath + sub + "/")
+    .filter((file) => file.endsWith(".js"));
+    console.log(commandFiles);
+
+    for (const file of commandFiles) {
+        const filePath = path.join(`${commandsPath}/${sub}/${file}`);
+        // console.log("Filepath:",filePath);
+        const command = require(filePath);
+    
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+            );
+        }
+    }
+})
+
+
+
+
+// const folders = readdir("./commands/");
+// folders.forEach(subFolder => {
+//     const commandFiles = fs.readdirSync('./commands/' + subFolder + "/").filter(file => file.endsWith('.js'));
+//     console.log(commandFiles);
+//     for(const file of commandFiles) {
+//         const command = require(`./commands/${subFolder}/${file}`);
+//         if("data" in command && "execute" in command) {
+//             client.commands.set(command.data.name, command);
+//         } else {
+//             console.log(`[WARNING] The command at ${commandFiles} is missing a required "data" or "execute" property.`);
+//         }
+//     }
+// })
+
+
+
+// client.on(Events.InteractionCreate, async (interaction) => {
+//     if (!interaction.isChatInputCommand()) return;
+
+//     const command = interaction.client.commands.get(interaction.commandName);
+
+//     if (!command) {
+//         console.error(
+//             `No command matching ${interaction.commandName} was found.`
+//         );
+//         return;
+//     }
+
+//     try {
+//         await command.execute(interaction);
+//     } catch (error) {
+//         console.error(error);
+//         await interaction.reply({
+//             content: 'There was an error while executing this command!',
+//             ephemeral: true,
+//         });
+//     }
+
+//     console.log(interaction);
+// });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+// client.once(Events.ClientReady, (c) => {
+//     console.log(`Ready!`);
+// });
+
+// Log in to Discord with your client's token
+client.login(process.env.TOKEN);
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,35 +1,48 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('node:fs');
-const dotenv = require('dotenv');
+const { REST, Routes } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
+const commandsPath = path.join(__dirname, "./commands/");
+let folders = fs.readdirSync("./commands/");
+folders.forEach((sub) => {
+    const commandFiles = fs
+        .readdirSync("./commands/" + sub + "/")
+        .filter((file) => file.endsWith(".js"));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const command = require(`./commands/${sub}/${file}`);
+        commands.push(command.data.toJSON());
+    }
+});
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 // and deploy your commands!
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    try {
+        console.log(
+            `Started refreshing ${commands.length} application (/) commands.`
+        );
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.clientId, process.env.guildId),
-			{ body: commands },
-		);
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.clientId,
+                process.env.guildId
+            ),
+            { body: commands }
+        );
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
+        console.log(
+            `Successfully reloaded ${data.length} application (/) commands.`
+        );
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
 })();
