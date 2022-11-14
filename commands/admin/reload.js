96bd061262033f505dbd98f4a2bd4d4fc05db217
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-11-14 15:02:36 +0800

    Implementing reload.js

--- commands/admin/reload.js
+++ commands/admin/reload.js
@@ -1,10 +1,44 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {
+    SlashCommandBuilder,
+    PermissionsBitField,
+    REST,
+    Routes,
+} = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('reload')
-        .setDescription('Replies with Pong!'),
+        .setName("reload")
+        .setDescription("Reloads the commands [Discord Bot Owner Only]!")
+        .addStringOption((option) => {
+            return option
+                .setName("category")
+                .setRequired(true)
+                .setDescription("The subfolder name");
+        })
+        .addStringOption((option) => {
+            return option
+                .setName("command")
+                .setRequired(true)
+                .setDescription("The command name");
+        }),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        if (
+            !interaction.member.permissions.has(
+                PermissionsBitField.Flags.KickMembers
+            )
+        ) {
+            return await interaction.reply(
+                "You do not have permission to use this command."
+            );
+        }
+        await interaction.deferReply();
+        const argCategory = interaction.options.getString("category");
+        const argCommand = interaction.options.getString("command");
+
+        await interaction.editReply(`Reloaded! ${argCategory} , ${argCommand}`);
     },
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -31,12 +31,9 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
-            Routes.applicationGuildCommands(
-                process.env.clientId,
-                process.env.guildId
-            ),
-            { body: commands }
-        );
+			Routes.applicationCommands(process.env.clientId),
+			{ body: commands },
+		);
 
         console.log(
             `Successfully reloaded ${data.length} application (/) commands.`
