828f3bcdbfe6ec6c6097ae58671e4a134a995d71
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-11-21 04:29:23 +0800

    Do db implementation stuff

--- src/dailyshots.js
+++ src/dailyshots.js
@@ -112,4 +112,6 @@ client.on("messageCreate", (message) => {
 
 // Log in to Discord with your client's token
 client.login(token);
-connect()
+(async () => {
+    await connect(dbToken).catch(err => console.error(err));
+})();
--- src/functions/handlers/eventHandler.js
+++ src/functions/handlers/eventHandler.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { connection } = require('mongoose');
 
 module.exports = (client) => {
     client.eventHandler = async () => {
@@ -22,7 +23,17 @@ module.exports = (client) => {
                         }
                     }
                     break;
-
+                case "mongo":
+                    for(const file of eventFiles) {
+                        const event = require(`../../events/${folder}/${file}`);
+                        if(event.once) {
+                            connection.once(event.name, (...args) => event.execute(...args, client));
+                        }
+                        else {
+                            connection.on(event.name, (...args) => event.execute(...args, client));
+                        }
+                    }
+                break;
                 default:
                     break;
             }
